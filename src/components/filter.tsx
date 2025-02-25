33ad88292f92ad7e6eb4493f65a4780376520141
Warllei Martins <warllei.s.martins@hotmail.com>
2025-02-25 13:28:29 -0300

    feat: agora são aceitos multiplos campos no input de texto

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,26 +1,46 @@
 import { Employee } from "../@types/types";
 import { FaSearch } from "react-icons/fa";
+import { IoFilter } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 import "./style.css"
+import { useState } from "react";
+import { Checkbox, FormControlLabel } from "@mui/material";
 
 export default function FilterComponent({ employees, setFilter }: { employees: Employee[], setFilter: React.Dispatch<React.SetStateAction<Employee[]>> }) {
 
+    const [showSearch, setShowSearch] = useState<boolean>(false);
+    const [search, setSearch] = useState<string[]>([]);
     const filterEmployees = (text: string) => {
-        const filtered = employees.filter(employee =>
-            employee.job.toLowerCase().includes(text.toLowerCase()) ||
-            employee.name.toLowerCase().includes(text.toLowerCase()) ||
-            employee.phone.toLowerCase().includes(text.toLowerCase()) ||
-            new Date(employee.admission_date).toLocaleDateString().includes(text.toLowerCase()));
+        const filtered = employees.filter(employee => {
+            return text.split(',').every(t => {
+                return ['name', 'job', 'phone', 'admission_date'].some(s => {
+                    switch (s) {
+                        case "name":
+                            return employee.name.toLowerCase().includes(t.trim().toLowerCase());
+                        case "job":
+                            return employee.job.toLowerCase().includes(t.trim().toLowerCase());
+                        case "phone":
+                            return employee.phone.toLowerCase().includes(t.trim().toLowerCase());
+                        case "admission_date":
+                            return new Date(employee.admission_date).toLocaleDateString().includes(t.trim().toLowerCase());
+                        default:
+                            return false;
+                    }
+                })
+            });
+        });
         setFilter(filtered);
     }
+
+    console.log(search)
 
     return (
         <div>
 
             <div className="container-input">
                 <div className="search-icon"> <FaSearch color="#fff" size={17} /></div>
-                <input className="input" type="text" onChange={e => filterEmployees(e.target.value)} />
+                <input placeholder="Ex: Maria, Front-end, 14/03/2020, 5557894561230" className="input" type="text" onChange={e => filterEmployees(e.target.value)} />
             </div>
-
         </div>
     )
 }
